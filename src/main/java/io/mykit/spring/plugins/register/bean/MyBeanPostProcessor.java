b4d68772a4e178d13cf6f9735316bbb9afa2ffbd
binghe_001 <1028386804@qq.com>
2020-06-24 23:52:07 +0800

    @PostConstruct注解和@PreDestroy注解

---
+++ src/main/java/io/mykit/spring/plugins/register/bean/MyBeanPostProcessor.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2020-9999 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.mykit.spring.plugins.register.bean;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author binghe
+ * @version 1.0.0
+ * @description 测试BeanPostProcessor
+ */
+@Component
+public class MyBeanPostProcessor implements BeanPostProcessor, Ordered {
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("调用了postProcessBeforeInitialization方法，beanName = " + beanName + ", bean = " + bean);
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("调用了postProcessAfterInitialization，beanName = " + beanName + ", bean = " + bean);
+        return bean;
+    }
+
+    @Override
+    public int getOrder() {
+        return 3;
+    }
+}
